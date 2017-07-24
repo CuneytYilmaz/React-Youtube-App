c5bf12e49bd3a37824888a40eb168c3a643a9847
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-25 00:35:04 +0300

    SearchBar, VideoDetails, VideoList and VideoListItem components created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () => {
+  return <div>Hello World!</div>;
+};
 
-ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
