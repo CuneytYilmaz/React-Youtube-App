c8088b76e90cc6faab05c31b595dceb17ec4d152
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-25 00:53:53 +0300

    youtube-api-search api key added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const API_KEY = "AIzaSyC3V_nTKGNmINn0XAc_s5TXo-62Xt5n0zI";
+
 const App = () => {
   return <div>Hello World!</div>;
 };
