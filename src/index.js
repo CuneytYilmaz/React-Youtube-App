b4ebd39fbdb4bf64aed6ed11aa8696e9c307da86
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-25 01:15:40 +0300

    SearchBar component completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SearchBar from "./components/search_bar";
+
 const API_KEY = "AIzaSyC3V_nTKGNmINn0XAc_s5TXo-62Xt5n0zI";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector(".container"));
